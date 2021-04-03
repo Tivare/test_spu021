52892f1c33d85552380fdf361e666ef30525b1b8
Khylko Andrii <hilko_a@itstep.academy>
2021-04-03 12:02:02 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <cmath>
+#include"sum.h"
+#include<string>
+#include"dif.h"
+using namespace std;
+
+int main()
+{
+
+	cout<<"Hello World!"<<endl;
+	cout<<"Dif = "<<dif(23,12)<<endl;
+}
