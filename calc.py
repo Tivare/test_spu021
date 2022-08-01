dc50b558db42be58ce1b01f078c2815edb725627
Khylko Andrii <hilko_a@itstep.academy>
2022-08-01 20:27:54 +0300

    add python files

---
+++ calc.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+	return a+b
+
+def dif(a,b):
+	return a-b
+
+def prod(a,b):
+	return a*b
---
+++ main.py
@@ -0,0 +1,15 @@
+from calc import sum
+from calc import dif
+import math
+from calc import prod
+
+
+def main():
+	print("Hello")
+	print(f"Sum = {sum(12,23)}")
+	print(f"Dif = {dif(12,23)}")
+	print(f"Pow = {math.pow(12,2)}")
+	print(f"Prod = {prod(12,23)}")
+
+if __name__ == "__main__":
+	main()
